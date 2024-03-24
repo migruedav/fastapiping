fec9762a77ca725e5ff1f1e4b598281d4519e5b4
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 21:09:09 -0600

    Agregado soporte para depuración remota en Chrome

--- sel.py
+++ sel.py
@@ -6,6 +6,7 @@ def sel():
     try:
         options = Options()
         options.add_argument("--headless")
+        options.add_argument("--remote-debugging-port=9222")
         options.binary_location = "/usr/bin/google-chrome"
         driver = webdriver.Chrome(options=options)
 
