49dee7eae7a4d0d275595c1941656832abebd91a
Miguel Rueda <migruedav@hotmail.com>
2024-03-23 21:06:21 -0600

    Agregada ubicación binaria de Google Chrome a las opciones de Selenium

--- sel.py
+++ sel.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 def sel():
     options = Options()
     options.add_argument("--headless")
+    options.binary_location = "/usr/bin/google-chrome"
     driver = webdriver.Chrome(options=options)
 
     return {"Ada": "QRCT"}
